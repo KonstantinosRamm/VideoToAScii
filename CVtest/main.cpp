a54430a712764637e8608077f020e3f771db0fef
KonstantinosRamm <ramkwnstantinos@gmail.com>
2025-03-16 14:13:34 +0200

    First Commit.Setup openCv and test

---
+++ CVtest/main.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+
+
+int main(void)
+{
+    cv::Mat image = cv::imread("wall.jpg");
+
+    if (image.empty())
+    {
+        std::cerr << "Error could not open image!" << std::endl;
+        return -1;
+    }
+
+    cv::Mat grayImage;
+    cv::cvtColor(image,grayImage,cv::COLOR_BGR2GRAY);
+
+    //display original and the modified image
+    cv::imshow("Original image",image);
+    cv::imshow("Modified image",grayImage);
+    //wait user input to exit
+
+    cv::waitKey(0);
+
+    return 0;
+
+    
+}
