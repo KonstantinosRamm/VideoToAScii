3fd347598a962cb2307a68601bcd19971ec68688
KonstantinosRamm <ramkwnstantinos@gmail.com>
2025-03-18 16:51:04 +0200

    initial commit : ensured opencv is installed

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,2 @@
-#include <iostream>
-#include <opencv2/highgui.hpp>
-#include <chrono>
-#include <thread>
 
-
-int main(int argc,char * argv[])
-{
-    //read video (hardcoded for now to sample/sample.txt)
-    cv::VideoCapture video("sample/sample.mp4");
-    if(!video.isOpened())
-    {
-        std::cerr << "[ERROR] opening video\n";
-        return -1;
-    }
-
-
-    cv::Mat frame;
-    //get fps
-    double fps = video.get(cv::CAP_PROP_FPS);
-
-    video.release();
-    return 0;
-}
 
