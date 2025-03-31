d8886e2e3ec60e95c1de19c303a2f1001d4ec713
KonstantinosRamm <ramkwnstantinos@gmail.com>
2025-03-31 16:46:26 +0300

    small correction to compilation to name the program from main to videoToAscii and updated README

--- README.md
+++ README.md
@@ -57,7 +57,7 @@ cd VideoToAscii
 # Run
 
 ```bash
-./main
+./videoToAscii
 ```
 
 
--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ -o main src/*.cpp -I./include `pkg-config --cflags --libs opencv4`
+g++ -o videoToAscii src/*.cpp -I./include `pkg-config --cflags --libs opencv4`
 
